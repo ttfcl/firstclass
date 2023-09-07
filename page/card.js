228b0eaf2e966d395123346f2ca57c0dae6132cc
ttfcl <ktx1016@naver.com>
2023-09-07 14:35:43 +0900

    r13123

--- page/card.js
+++ page/card.js
@@ -1,5 +1,5 @@
 
 if(localStorage.getItem("userDisplayName") === null) {
-    localStorage.setItem("alret", "alret")
+    alert("프로필 및 서비스 확인을 원하시면 카카오톡으로 문의주세요.")
     location.href = "../login.html"
 }
